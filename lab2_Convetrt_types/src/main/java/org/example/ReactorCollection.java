a30ab4309ace03276c52ae74ce0c9f58ae73e99b
kkolbassa <KZSBVP@GMAIL.COM>
2023-03-31 15:15:30 +0300

    add class as collection

---
+++ lab2_Convetrt_types/src/main/java/org/example/ReactorCollection.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.example.reactors.Reactor;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.util.ArrayList;
+
+public class ReactorCollection {
+    private ArrayList<Reactor> reactors = null;
+
+    public ReactorCollection() {
+    }
+
+    public ArrayList<Reactor> getReactors() {
+        return reactors;
+    }
+
+    public void setReactors(ArrayList<Reactor> reactors) {
+        this.reactors = reactors;
+    }
+
+    public DefaultMutableTreeNode addInfo2GUI() {
+        DefaultMutableTreeNode main = new DefaultMutableTreeNode("Реакторы");
+        for (Reactor reactor: reactors) {
+            main.add(reactor.getNode());
+        }
+        return main;
+    }
+}

1f01541bff1d007775e25fcf4adfef5c4a3bf93e
kkolbassa <KZSBVP@GMAIL.COM>
2023-03-31 16:56:46 +0300

    add inition

--- lab2_Convetrt_types/src/main/java/org/example/Main.java
+++ lab2_Convetrt_types/src/main/java/org/example/Main.java
@@ -1,16 +1,11 @@
 package org.example;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ReactorParserYaml rp = new ReactorParserYaml();
-        ArrayList<Reactor> reactors = rp.parse("./data/Reactors_yaml.yaml");
-       // ArrayList<Reactor> reactors = rp.parse("./data/Reactors_json.json");
-       for (Reactor reactor: reactors) {
-            System.out.println(reactor.getClassReactor());
-        }
+
+        new JFrameProgram().setVisible(true);
     }
 }
